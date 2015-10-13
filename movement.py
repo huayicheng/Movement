0c1aabb95ffaed6a51fa16edcfd25f17b043193b
huayicheng <2313948181@qq.com>
2015-10-13 21:48:36 +0800

    +differentiate length

--- movement.py
+++ movement.py
@@ -6,8 +6,34 @@
 
 import os
 import shutil as sut
+from xml.dom import minidom as md
+
+def get_string_nodes_from_xml(file_abs_path,string_tag_name):
+    """
+@param file_abs_path:xml file absolute path
+@return a list consis of all string nodes in the param xml file
+"""
+
+    doc = md.parse(file_abs_path)
+    root = doc.documentElement
+    return root.getElementsByTagName(string_tag_name)
+
+def do_copy_job(string_file_name,values_dir_path,values_en_dir_path,string_tag_name):
+    """
+@param string_file_name whose data need to be copied
+@param values_dir_path the data source
+@param values_en_dir_path the data target
+"""
+
+    values_string_nodes = get_string_nodes_from_xml(values_dir_path + os.sep + string_file_name,string_tag_name)
+    print "len(values_string_nodes):",len(values_string_nodes)
+    values_en_string_nodes = get_string_nodes_from_xml(values_en_dir_path + os.sep + string_file_name,string_tag_name)
+    print "len(values_en_string_nodes):",len(values_en_string_nodes)
 
 if __name__ == "__main__":
+    #string tag name
+    STRING_TAG_NAME = "string"
+    STRING_TAG_NAME_ATTR = "name"
 
     #2 directories:one is from directory,the other is to directory
     VALUES_DIR_NAME = "values"
@@ -47,7 +73,26 @@ if __name__ == "__main__":
         else:
             print i,"exists in",VALUES_EN_DIR_NAME
 
-    
+    #now,create 2 dom trees for every strings xml in 2 values* directories
+    for i in all_strings_xml_files:
+        value_string_nodes = get_string_nodes_from_xml(".." + os.sep + VALUES_DIR_NAME + os.sep + os.path.basename (i),STRING_TAG_NAME)
+        print "there are",len(value_string_nodes),"string nodes in",".." + os.sep + VALUES_DIR_NAME + os.sep + os.path.basename (i)
+        #break;
+
+        value_en_string_nodes = get_string_nodes_from_xml(os.getcwd () + os.sep + os.path.basename (i),STRING_TAG_NAME)
+        print "there are",len(value_en_string_nodes),"string nodes in",os.getcwd () + os.sep + os.path.basename (i)
+        #if len(value_en_string_nodes) < 5:
+        #   for j in value_en_string_nodes:
+        #      print "the node's attribute is",j.getAttribute(STRING_TAG_NAME_ATTR)
+
+        #whether the 2 lengthes of lists are equal to each other
+        #if so,do nothing,if not,do copy job
+        #copy the data which exists in values folder string xml but not exists in values-en folder string xml to values-en folder string xml
+        if len(value_string_nodes) != len(value_en_string_nodes):
+            print "the node number in values folder is different from that in values-en folder:",i
+
+            #do copy job here
+            do_copy_job(os.path.basename (i),".." + os.sep + VALUES_DIR_NAME,os.getcwd (),STRING_TAG_NAME)
 
     #back
     os.chdir ("..")
